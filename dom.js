17b46dd57b24142d2705204e5705dadee0683bec
ctrl07 <skgaming@gmail.com>
2023-06-09 07:59:11 +0530

    DOM Assignment 7

--- dom.js
+++ dom.js
@@ -49,11 +49,91 @@
 // }
 
 // QuerySelector=> Grabs the top most element if multiple use QUERYSELECTORALL //
-let li=document.querySelectorAll('li');
-li[1].style.color='green';
+// let li=document.querySelectorAll('li');
+// li[1].style.color='green';
 
-let odd=document.querySelectorAll('li:nth-child(odd');
+// let odd=document.querySelectorAll('li:nth-child(odd');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green';
+// }
+
+// TRAVERSING THR DOM //
+
+let itemList= document.querySelector('#items');
+
+// //parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentNode.parentNode);
+
+//parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentElement.parentElement);
+
+// childNode
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+
+// //firstChild//
+// console.log(itemList.firstChild);
+// //firstElement Child //
+// console.log(itemList.firstElementChild);
+
+// //lastChild//
+// console.log(itemList.lastChild);
+// //lastElement Child //
+// console.log(itemList.lastElementChild);
+
+// // nextSibiling
+// console.log(itemList.nextSibling);
+// // nextElementSibiling
+// console.log(itemList.nextElementSibling);
+
+// // previousSibiling
+// console.log(itemList.previousSibling);
+// // previousElementSibiling
+// console.log(itemList.previousElementSibling);
+
+// createElement
+
+// create a div
+
+let newDiv=document.createElement('div');
+
+//Add class
+newDiv.className='hello';
+
+// Add id
+newDiv.id='hello1';
+
+// Add attribute
+newDiv.setAttribute('title','Hello Div');
+//console.log(newDiv);
+
+// CREATE TEXT NODE
+let newDivText=document.createTextNode('HEllo word');
+
+// add text to div
+newDiv.appendChild(newDivText);
+
+let container=document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+//console.log(newDiv);
+
+// add HEllo word before Item Lister
+
+container.insertBefore(newDiv,h1);
+
+
+// add HEllo word before Item 1
+let oldDiv=document.createElement('div');
+oldDiv.className='hellow';
+oldDiv.id='hello2'
+oldDiv.setAttribute('li','Hello Div1');
+let oldDivText=document.createTextNode('HEllo word');
+oldDiv.appendChild(oldDivText);
+let random=document.querySelector('ul .list-group-item');
+var li = document.querySelector('header ul li');
+random.insertBefore(oldDiv,li);
