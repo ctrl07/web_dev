08a234364d13bf3a62f93c07c424978ae6c765d9
ctrl07 <skgaming@gmail.com>
2023-06-20 20:55:20 +0530

    Added the Edit button (assignment 9 DOM Maniulation)

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let form=document.getElementById('addForm');
 let itemList= document.getElementById('items');
+let filter = document.getElementById('filter');
 
 //Form submit event
 form.addEventListener('submit',addItem);
 //Delete event
 itemList.addEventListener('click', removeItem);
+//Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add Item
 function addItem(e){
@@ -12,7 +15,8 @@ function addItem(e){
      
     // get input value
 
-    let newItem=document.getElementById('item').value;
+    let newItem=document.getElementById('item',).value;
+    let newItem1=document.getElementById('item1',).value;
 
     // Create new li element
     let li = document.createElement('li');
@@ -22,17 +26,22 @@ function addItem(e){
 
     li.appendChild(document.createTextNode(newItem));
 
+    li.appendChild(document.createTextNode(newItem1));
+
     //Create del button
     let delBtn = document.createElement('button');
-
+    let edtBtn = document.createElement('button');
     //Add classes to button
     delBtn.className = 'btn btn-danger btn-sm float-right delete';
+    edtBtn.className = 'btn btn-primary btn-sm float-right edit';
 
     //Append text node
     delBtn.appendChild(document.createTextNode('X'));
+    edtBtn.appendChild(document.createTextNode('Edit'));
 
     //Append button to li
     li.appendChild(delBtn);
+    li.appendChild(edtBtn);
 
     //append li to list
     itemList.appendChild(li);
@@ -49,3 +58,24 @@ function removeItem(e){
         }
     }
 }
+
+//Filter Items
+
+function filterItems(e){
+    //convert to lowercase
+    let text= e.target.value.toLowerCase();
+    //get lis
+    let items=itemList.getElementsByTagName('li');
+
+    //Convert to array
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!= -1){
+            item.style.display='block';
+        }else {
+            item.style.display = 'none';
+        }
+    });
+
+}
+
