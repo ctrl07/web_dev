9ab9b0a5662bd9f5e85c3cf11b88a308c4618bad
ctrl07 <skgaming@gmail.com>
2023-06-21 02:06:49 +0530

    Log the user details to the browser window (Assignment 12 DOM Manipulation)

--- appointment_app/index.js
+++ appointment_app/index.js
@@ -5,13 +5,28 @@ function onsignup(event) {
     let phone=document.getElementById('phone').value;
     let time=document.getElementById('time').value;
 
+    //Create new list 
+    let listItem = document.createElement('li');
+    let textNode = document.createTextNode(fname+' '+email+' '+phone+' '+time);
+    listItem.appendChild(textNode);
+
+    //Append li to ul
+    let displayList = document.getElementById('item-group');
+    displayList.appendChild(listItem);
+
+    //Clear form fields 
+    document.getElementById('fname').value='';
+    document.getElementById('email').value='';
+    document.getElementById('phone').value='';
+    document.getElementById('time').value='';
+
+
     let userData = {
         name: fname,
         mail: email,
         phone: phone,
-        time: time
     }
 
-    localStorage.setItem('userData', JSON.stringify(userData));
+    localStorage.setItem(userData.mail, JSON.stringify(userData));
 };
 
